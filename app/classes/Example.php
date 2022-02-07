1dee3a6fa6886799ce36c54bcc936791fe81f9dc
NafisaNitu <nafisanitu240@gmail.com>
2022-02-07 17:41:58 +0600

    php folder upload

---
+++ app/classes/Example.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace App\classes;
+
+
+class Example
+{
+    public $i;
+
+    public function index(){
+//        for ($this->i = 45; $this->i > 22; $this->i--){
+//            echo $this->i. '<br>';
+//
+//        }
+
+//        $this->i = 0;
+//        while ($this->i < 20){
+//            echo $this->i . '<br>';
+//            $this->i++;
+//        }
+
+
+        $this->i = 10;
+        do{
+            echo $this->i . '<br>';
+            $this->i++;
+        } while ($this->i < 20);
+
+
+
+
+
+    }
+}
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once 'vendor/autoload.php';
+use App\classes\Example;
+
+$example = new Example();
+$example->index();
+
+
+
+?>
